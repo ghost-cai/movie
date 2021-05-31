2d893e714656089767d2491938237303f0d36cc0
ghost-cai <1786726227@qq.com>
2021-05-31 23:28:54 +0800

    2021/5/31
    修改了分类搜索部分

--- My/app/src/main/java/com/example/my/fragment/LeftFragment.java
+++ My/app/src/main/java/com/example/my/fragment/LeftFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.my.R;
 
@@ -23,12 +24,21 @@ public class LeftFragment extends Fragment {
     }
 
 
+    private String[] data={"爱情","科幻","动作","西部","恐怖","戏剧"};
+    /*定义一个动态数组*/
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_left, container, false);
+        View view=inflater.inflate(R.layout.fragment_left, container, false);
+
+        ArrayAdapter<String> adapterL=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_expandable_list_item_1,data);
+        ListView listViewL=view.findViewById(R.id.lv1);
+        listViewL.setAdapter(adapterL);
+
+        return view;
     }
 }
